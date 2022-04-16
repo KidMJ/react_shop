3e7d7c6c96f3e88d08b69d4df470130aa8fd5e31
KidMJ <cchokid@gmail.com>
2022-04-17 01:24:37 +0900

    detail_page_update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
+import Detail from './Detail.js'
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -18,8 +19,8 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link href="#link"><Link to="/detail">Detail</Link></Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -33,6 +34,7 @@ function App() {
     </Navbar>
 
    
+   <Switch>
 
     <Route exact path="/">
       <div className="jumbotron">
@@ -54,26 +56,22 @@ function App() {
     </div>
 
     </Route>
-    <Route path="/detail">
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-6'>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="100%" />
-          </div>
-          <div className='col-md-6 mt-4'>
-            <h4 className='pt-5'>상품명</h4>
-            <p>상품 설명</p>
-            <p>120000원</p>
-            <button className='btn btn-danger'>주문하기</button>
-          </div>
-        </div>
-      </div>
+
+    <Route path="/detail/:id">
+      <Detail shoes={shoes}/>
     </Route>
 
+
+    <Route path="/:id">
+      <div>아무거나적었을때</div>
+    </Route>
+  </Switch>
 
   </div>
   );
 }
+
+
 
 function Card(props,i){
   return ( 
